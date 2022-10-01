c1d7b0b670b63b3d43d43a91995ef53842e37bb9
D1plo1d <thatotherdude@gmail.com>
2022-10-01 19:26:51 -0400

    doc: Added a README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+A no_std ArtNet 4 implementation for creating microcontroller-based ArtNet Nodes.
+
+## Implemented
+
+✓ Node Discovery:
+    ✓ ArtPoll: Parser
+    ✓ ArtPollReply: Serialization
+✓ DMX Lighting Control:
+    ✓ ArtDMX: Parser
+✓ Re-Programming:
+    ✓ ArtCommand: Parser
+
+## Not Implemented
+
+Literally everything else. Pull Request are welcome.
+
+## ArtNet
+
+ArtNet 4 Spec: https://artisticlicence.com/WebSiteMaster/User%20Guides/art-net.pdf
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,4 @@
 #![no_std]
-/// A no_std ArtNet 4 implementation for creating microcontroller-based ArtNet Nodes.
-///
-/// Implemented:
-///
-/// ✓ Node Discovery:
-///     ✓ ArtPoll: Parser
-///     ✓ ArtPollReply: Serialization
-/// ✓ DMX Lighting Control:
-///     ✓ ArtDMX: Parser
-/// ✓ Re-Programming:
-///     ✓ ArtCommand: Parser
-///
-/// Not Implemented: Literally everything else. Pull Request are welcome.
-///
-/// ArtNet 4 Spec: https://artisticlicence.com/WebSiteMaster/User%20Guides/art-net.pdf
 extern crate tiny_artnet_bytes_no_atomic as bytes;
 
 mod poll_reply;
